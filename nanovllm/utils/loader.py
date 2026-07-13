b672c303e3d116359c11d231bbdba55a42c7da7f
HC-Skade <1486220883@qq.com>
2026-07-13 20:40:32 +0800

    fix: map packed checkpoint paths by component

--- nanovllm/utils/loader.py
+++ nanovllm/utils/loader.py
@@ -14,10 +14,13 @@ def load_model(model: nn.Module, path: str):
     for file in glob(os.path.join(path, "*.safetensors")):
         with safe_open(file, "pt", "cpu") as f:
             for weight_name in f.keys():
-                for k in packed_modules_mapping:
-                    if k in weight_name:
-                        v, shard_id = packed_modules_mapping[k]
-                        param_name = weight_name.replace(k, v)
+                weight_path = weight_name.split(".")
+                for source_name, mapping in packed_modules_mapping.items():
+                    if source_name in weight_path:
+                        target_name, shard_id = mapping
+                        packed_path = weight_path.copy()
+                        packed_path[packed_path.index(source_name)] = target_name
+                        param_name = ".".join(packed_path)
                         param = model.get_parameter(param_name)
                         weight_loader = getattr(param, "weight_loader")
                         weight_loader(param, f.get_tensor(weight_name), shard_id)
